05cab91de6817d642cbd7cfdd15daaf9a07b5b84
Xander-Junior <xanderbyiadom278@gmail.com>
2023-06-15 21:52:04 -0700

    Fix Betty warnings

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,9 +9,9 @@
  */
 int main(void)
 {
-    char *q = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *q = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-    write(2, q, 59);
+	write(2, q, 59);
 
-    return (1);
+	return (1);
 }
