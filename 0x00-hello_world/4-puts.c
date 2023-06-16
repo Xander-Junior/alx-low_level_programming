585fda4121f73cfafc7376ac55c89f389b095954
Xander-Junior <xanderbyiadom278@gmail.com>
2023-06-15 21:29:23 -0700

    Fix Betty warnings

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints the message "Programming is like building a multilingual puzzle"
+ *              to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+    puts("\"Programming is like building a multilingual puzzle\"");
+    return (0);
 }
+
