08ff00f250a67e0e95919ecfbb28b9df64b63576
Xander-Junior <xanderbyiadom278@gmail.com>
2023-06-21 12:09:52 -0700

    Fixing errors

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,41 +13,35 @@ void print_times_table(int n)
 
 	for (i = 0; i <= n; i++)
 	{
-		for (j = 0; j <= n; j++)
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+
+		for (j = 1; j <= n; j++)
 		{
 			result = i * j;
 
-			if (j != 0)
+			if (result < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else if (result < 100)
+			{
+				_putchar(' ');
+			}
+
+			_putchar(result / 100 + '0');
+			_putchar((result / 10) % 10 + '0');
+			_putchar(result % 10 + '0');
+			
+			if (j < n )
 			{
 				_putchar(',');
 				_putchar(' ');
-
-				if (result < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(result + '0');
-
-				}
-				else if (result < 100)
-				{
-					_putchar(' ');
-					_putchar(result / 10 + '0');
-					_putchar(result % 10 + '0');
-
-				}
-				else
-				{
-					_putchar(result / 100 + '0');
-					_putchar((result / 10) % 10 + '0');
-					_putchar(result % 10 + '0');
-				}
-			}
-			else
-			{
-				_putchar(result + '0');
 			}
 		}
-		_putchar('\n');
-	}
+
+			_putchar('\n');
+		}
 }
