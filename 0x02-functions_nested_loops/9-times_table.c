02b56cbd359f8d1bdab29fa75fcb5e118b0fef3c
Xander-Junior <xanderbyiadom278@gmail.com>
2023-06-20 23:01:02 -0700

    Fix Betty warnings

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,33 +1,41 @@
 #include "main.h"
 
 /**
- * times_table - Prints the 9 times table
+ * times_table - Prints the modified version of the 9 times table
  */
 void times_table(void)
 {
-	int i, j, result;
+    	int i, j, k;
 
-	for (i = 0; i <= 9; i++)
-	{
-		for (j = 0; j <= 9; j++)
-		{
-			result = i * j;
+    	for (i = 0; i < 10; i++)
+    	{
+        	for (j = 0; j < 10; j++)
+        		{
+            			k = i * j;
 
-			if (j != 0)
-				_putchar(',');
-			if (result >= 10)
-			{
-				_putchar(' ');
-				_putchar(result / 10 + '0');
-				_putchar(result % 10 + '0');
-			}
-			else
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(result + '0');
-			}
-		}
-		_putchar('\n');
-	}
+            			if (j == 0)
+            			{
+                			_putchar(k + '0');
+            			}
+            			else
+            			{
+                			_putchar(',');
+                			_putchar(' ');
+
+                			if (k < 10)
+                			{
+                    				_putchar(' ');
+                    				_putchar(' ');
+                    				_putchar(k + '0');
+                			}
+                			else
+                			{
+                    				_putchar((k / 10) + '0');
+                    				_putchar((k % 10) + '0');
+                			}
+            			}
+        		}
+        		_putchar('\n');
+    	}
 }
+
