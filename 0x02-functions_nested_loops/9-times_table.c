4df5fa221aaedca0376c029fe2cad959f3ad9e9a
Xander-Junior <xanderbyiadom278@gmail.com>
2023-06-20 23:16:18 -0700

    Fix Betty warnings

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,41 +1,40 @@
 #include "main.h"
 
 /**
- * times_table - Prints the modified version of the 9 times table
- */
+ * times_table - print the table of times
+ *
+ * Rutern: nothing
+*/
 void times_table(void)
-{
-    	int i, j, k;
+	{
+		int i, j, k;
 
-    	for (i = 0; i < 10; i++)
-    	{
-        	for (j = 0; j < 10; j++)
-        		{
-            			k = i * j;
 
-            			if (j == 0)
-            			{
-                			_putchar(k + '0');
-            			}
-            			else
-            			{
-                			_putchar(',');
-                			_putchar(' ');
+		for (i = 0; i < 10; i++)
+		{
+			for (j = 0; j < 10; j++)
+			{
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				}
 
-                			if (k < 10)
-                			{
-                    				_putchar(' ');
-                    				_putchar(' ');
-                    				_putchar(k + '0');
-                			}
-                			else
-                			{
-                    				_putchar((k / 10) + '0');
-                    				_putchar((k % 10) + '0');
-                			}
-            			}
-        		}
-        		_putchar('\n');
-    	}
-}
 
+				if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
