68429928317ec80f466ac2ce69f8ea70fe802ebf
Xander-Junior <xanderbyiadom278@gmail.com>
2023-06-23 22:09:21 -0700

    Fix some bugs

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,31 +1,32 @@
 #include <stdio.h>
-/**
-* bigprime - biggest prime number
-* @n: int arg
-* Return: value prime-number largest
-*/
-int bigprime(unsigned long int n)
-{
-	long int i;
+#include <math.h>
 
-	for (i = 2; i <= n; i++)
-	{
-		if (n % i == 0)
-			n = n / i;
-		else
-			continue;
-	}
-	return (i - 1);
-}
 /**
-* main - entry point
-* Return: 0
-*/
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 	unsigned long int n = 612852475143;
-	unsigned long int l = bigprime(n);
+	unsigned long int i;
 
-	printf("%ld\n", l);
+	for (i = 2; i <= sqrt(n); i++)
+	{
+		while (n % i == 0)
+		{
+			n = n / i;
+		}
+	}
+
+	if (n > 2)
+	{
+		printf("%lu\n", n);
+	}
+	else
+	{
+		printf("%lu\n", i);
+	}
+
 	return (0);
 }
