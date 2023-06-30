5acedd150f931fac8de64a2b47a5895b4afd7160
Xander-Junior <xanderbyiadom278@gmail.com>
2023-06-29 23:09:52 -0700

    Write a function that prints a buffer.

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,6 +13,7 @@ int _putchar(char c);
 void print_number(int n);
 char *_strncpy(char *dest, char *src, int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 
 #endif /* MAIN_H */
