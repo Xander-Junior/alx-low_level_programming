99d05ac3ff9e35f7ac828da65cb3ef5dfe492ea7
Xander-Junior <xanderbyiadom278@gmail.com>
2023-06-29 22:57:10 -0700

    Add main.h file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,5 +12,7 @@ char *rot13(char *str);
 int _putchar(char c);
 void print_number(int n);
 char *_strncpy(char *dest, char *src, int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
 
 #endif /* MAIN_H */
