b8dcbd415d248fc66c7885ef0c21bc9b8ed2ebb1
Xander-Junior <xanderbyiadom278@gmail.com>
2023-07-04 22:53:12 -0700

    Write a function that prints the sum of the two diagonals of a square matrix of integers.

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,6 +10,7 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
+void set_string(char **s, char *to);
 
 /* Any other declarations or macro definitions */
 
