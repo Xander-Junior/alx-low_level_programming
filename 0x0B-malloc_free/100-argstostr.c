325fd2996ee0c9066efb38e1b1af96e8e8f8f395
Xander-Junior <xanderbyiadom278@gmail.com>
2023-07-11 22:25:03 -0700

    Correct error

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,56 +1,52 @@
 #include "main.h"
-/**
-* argstostr - a function that concatenates all the arguments of your program
-*@ac: count of args passed to the function
-*@av:array of arguments
-*
-*Return: pointer to the new string
-*/
+#include <stdlib.h>
 
+/**
+ * argstostr - Concatenates all the arguments of the program.
+ * @ac: The argument count.
+ * @av: The argument vector.
+ *
+ * Return: Pointer to the new concatenated string,
+ *         or NULL on failure.
+ */
 char *argstostr(int ac, char **av)
 {
-		char *new_string = NULL;
-		int k = 0, i = ac, j, sum = 0, temp = 0;
+	char *str;
+	int total_length = 0, i, j, k = 0;
 
-		if (ac == 0 || av == NULL)
-			return (NULL);
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
 
-		while (ac--)
-			sum += (len(av[ac]) + 1);
-		new_string = (char *) malloc(sum + 1);
-
-		if (new_string != NULL)
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
 		{
-			while (k < i)
-			{
-				for (j = 0; av[k][j] != '\0'; j++)
-					new_string[j + temp] = av[k][j];
-				new_string[temp + j] = '\n';
-				temp += (j + 1);
-				k++;
-			}
-			new_string[temp] = '\0';
+			total_length++;
 		}
-		else
-		{
-			return (NULL);
-		}
-		return (new_string);
-}
+	}
+	total_length++;
 
-/**
-*len - returns length of str
-*@str: string counted
-*Return: returns the length
-*/
-int len(char *str)
-{
-		int len = 0;
+	str = malloc(sizeof(char) * (total_length + 1));
 
-		if (str != NULL)
+	if (str == NULL)
+	{
+		 return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (i = 0; i < ac; i++)
 		{
-			while (str[len])
-				len++;
+			str[k] = av[i][j];
 		}
-	return (len);
+		if (i != ac - 1)
+		{
+			str[k++] = '\n';
+		}
+	}
+
+	str[k] = '\0';
+
+	return (str);
 }
