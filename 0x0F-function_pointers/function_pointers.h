63ca3eba9e01478ac6aa58cec6a4a75db07a8294
Xander-Junior <xanderbyiadom278@gmail.com>
2023-07-19 20:08:49 -0700

    Write a function that prints a name.

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/* Include any necessary header files here */
+
+/* Function prototype for the print_name function */
+void print_name(char *name, void (*f)(char *));
+
+/* Add any other function prototypes or declarations here if needed */
+
+#endif /* FUNCTION_POINTERS_H */
